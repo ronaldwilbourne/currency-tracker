3ccf5a8001654b70cd4d558d3aa57bbc8e5325a9
ronnie96 <ronaldwilbourne@gmail.com>
2022-10-31 10:32:45 -0400

    axios on api

--- src/components/Content.js
+++ src/components/Content.js
@@ -9,7 +9,10 @@ const Content = () => {
     useEffect(()=> {
         axios ({
             method:'Get',
-            url:''
+            url:'https://www.scorebat.com/video-api/v3/feed/?token=MzE0NjdfMTY2NzIyNjY0Ml9kZmI3NTRlZTFkMjU1NDFkNzU0MjAyNGY0ZjlmMWJkZmJmMDYyN2Y3'
+            
+        }).then(res=> {
+          console.log(res.data);
         })
     },[] )
 
