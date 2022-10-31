a7a6c4cb21916c62998cb266b4423b6a4d8f926d
ronnie96 <ronaldwilbourne@gmail.com>
2022-10-31 08:59:33 -0400

    footer and navbar

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../App'
+
+const Footer = () => {
+  return <div className='footer'>
+    <p> Football Videos App 2022&copy</p></div>
+  
+}
+
+export default Footer
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../App'
+
+const Navbar = () => {
+  return  <div className='navbar'>
+    <h1> Football videos app ⚽</h1>
+    </div>
+  
+}
+
+export default Navbar
